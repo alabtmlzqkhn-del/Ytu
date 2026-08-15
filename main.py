f786fba7c2545eba304da81a5835697cf17ba1d9
alabtmlzqkhn-del <alabtmlzqkhn@gmail.com>
2026-08-15 18:28:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from pyrogram import *
 
 # 1. تعريف المتغيرات الأساسية فوراً
 to_config = "" 
-token = os.getenv('BOT_TOKEN') or input('Bot Token: ')
-owner_id = os.getenv('OWNER_ID') or input('Owner ID: ')
+token = os.getenv('8516176029:AAEiCQLVCf1HYoB4WOgwdK_cCAwsJXGyR2g') or input('Bot Token: ')
+owner_id = os.getenv('8065884629') or input('Owner ID: ')
 Dev_Neptune = token.split(':')[0]
 
 
 # 2. محاولة الاتصال بـ Redis
 try:
-    r = redis.Redis.from_url(os.getenv('REDIS_URL', 'redis://localhost:6379'), decode_responses=True)
+    r = redis.Redis.from_url(os.getenv('REDIS_URL', 'https://www.tiktok.com/@rmaxatee/video/7666134427214384404?_r=1&u_code=ej378a8a51b0ic&preview_pb=0&sharer_language=ar&_d=f09d18ag72h7ej&share_item_id=7666134427214384404&source=h5_m&timestamp=1786796927&user_id=7476050567253869584&sec_user_id=MS4wLjABAAAAREz3RNKeIhkgmx1s-MKlbne9e07a28bRRhXjgGWALmsiXAwASGXJftwBCPJixZLN&social_share_type=0&utm_source=copy&utm_campaign=client_share&utm_medium=android&share_iid=7673958488049272597&share_link_id=86c9d5ca-0b50-49be-8e80-97779def301c&share_app_id=1233&ugbiz_name=MAIN&ug_btm=b2001&sp_root_share_link_id=86c9d5ca-0b50-49be-8e80-97779def301c&link_reflow_popup_iteration_sharer=%7B%22click_empty_to_play%22%3A1%2C%22dynamic_cover%22%3A1%2C%22follow_to_play_duration%22%3A-1.0%2C%22profile_clickable%22%3A1%7D&panel_source_v2=share_panel&share_enter_from=homepage_hot&item_author_type=2&enable_checksum=1&sp_level=1&sp_root_u=ej378a8a51b0ic&sp_root_d=f09d18ag72h7ej'), decode_responses=True)
     r.ping()
 except:
     print("[-] Error: Redis is not running. Please run: sudo systemctl start redis-server")
